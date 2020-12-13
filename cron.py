06736c75bdb586194e3f292fe5acce20264022ff
Charles Siu <chunheisiu@gmail.com>
2020-12-13 23:13:04 +0800

    Add remove cron job

--- cron.py
+++ cron.py
@@ -1,29 +1,70 @@
 import json
 import os
+import sys
 
 from crontab import CronTab
 
 from log_helper import get_logger
 
-# Load config
-with open('config.json') as config_file:
-    config = json.load(config_file)
-
 # Init logger
 logger = get_logger(__name__)
 
-# Get current directory
-path = os.getcwd()
 
-# Set up CronTab
-job_comment = config['CRON']['COMMENT']
-job_pattern = config['CRON']['SCHEDULE']
+def add_job(config: dict):
+    """
+    Add a cron job to CronTab.
 
-cron = CronTab(user=config['CRON']['USERNAME'])
-cron.remove_all(comment=job_comment)
-job = cron.new(command=f'cd {path} && {path}/{config["CRON"]["VENV"]}/bin/python {path}/main.py',
-               comment=job_comment)
-job.setall(job_pattern)
-cron.write()
+    :param config: config dict
+    :return:
+    """
+    # Get current directory
+    path = os.getcwd()
 
-logger.info(f'Finished writing cron {job_comment} with pattern {job_pattern}')
+    # Extract config params
+    job_comment = config['CRON']['COMMENT']
+    job_pattern = config['CRON']['SCHEDULE']
+
+    # Set up CronTab
+    cron = CronTab(user=config['CRON']['USERNAME'])
+
+    # Remove existing job and add job
+    cron.remove_all(comment=job_comment)
+    job = cron.new(command=f'cd {path} && {path}/{config["CRON"]["VENV"]}/bin/python {path}/main.py',
+                   comment=job_comment)
+    job.setall(job_pattern)
+    cron.write()
+
+    logger.info(f'Added cron job {job_comment} with pattern {job_pattern}')
+
+
+def remove_job(config: dict):
+    """
+    Remove a cron job from CronTab.
+
+    :param config: config dict
+    :return:
+    """
+    # Extract config param
+    job_comment = config['CRON']['COMMENT']
+
+    # Set up CronTab and remove job
+    cron = CronTab(user=config['CRON']['USERNAME'])
+    cron.remove_all(comment=job_comment)
+    cron.write()
+
+    logger.info(f'Removed cron job {job_comment}')
+
+
+# Load config
+with open('config.json') as config_file:
+    config_file = json.load(config_file)
+
+# Determine what operation to perform according to sys args
+if len(sys.argv) == 1:
+    add_job(config_file)
+elif len(sys.argv) == 2:
+    op = sys.argv[1].lower()
+    if op == 'add':
+        add_job(config_file)
+    elif op == 'remove':
+        remove_job(config_file)
