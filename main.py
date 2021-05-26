1f5fda4b7c270b1221759914bacb20c7fcc99a8c
Charles Siu <chunheisiu@gmail.com>
2021-05-26 21:25:43 +0800

    Update to use instagrapi

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from typing import Dict
 
 import requests
 import zhon.hanzi
-from instagram_private_api import Client, ClientTwoFactorRequiredError, ClientError
+from instagrapi import Client
+from instagrapi.exceptions import ClientError, TwoFactorRequired
 
-from json_helper import to_json, from_json
 from log_helper import get_logger
 
 # Load config
@@ -51,7 +51,7 @@ def get_lastfm_user_sotw(username: str) -> Dict[str, str]:
     :param username: username of the user
     :return: Dict of `{artist, track}`
     """
-    LAST_FM_URL = f'http://ws.audioscrobbler.com/2.0/'
+    LAST_FM_URL = f'https://ws.audioscrobbler.com/2.0/'
     last_fm_params = {'method': 'user.gettoptracks',
                       'format': 'json',
                       'user': username,
@@ -60,15 +60,17 @@ def get_lastfm_user_sotw(username: str) -> Dict[str, str]:
                       'period': '7day'}
 
     # Retrieve the SOTW from Last.fm
+    logger.info('Retrieving Song of the Week from Last.fm...')
     r = requests.get(LAST_FM_URL, params=last_fm_params, headers={'User-Agent': 'Mozilla/5.0'})
     track = r.json()['toptracks']['track']
-    logger.info('Retrieved Song of the Week from Last.fm')
+
     if len(track) < 1:
         raise IndexError('At least one track is expected to return from Last.fm')
     track = track[0]
     # Extract the artist and tack name from the JSON response
     track_dict = {'artist': track['artist']['name'], 'name': track['name']}
     logger.info(f'Song of the Week: {track_dict}')
+    logger.info('Retrieved Song of the Week from Last.fm')
     return track_dict
 
 
@@ -88,20 +90,6 @@ def format_sotw(sotw: Dict[str, str]) -> str:
     return f'\n{sotw_str}' if sotw_str_len > 25 else sotw_str
 
 
-def onlogin_callback(api, new_settings_file):
-    """
-    Callback function for Instagram client login.
-
-    :param api: Instagram client
-    :param new_settings_file: settings file path
-    :return:
-    """
-    cache_settings = api.settings
-    with open(new_settings_file, 'w') as outfile:
-        json.dump(cache_settings, outfile, default=to_json)
-        logger.info(f'Settings saved: {new_settings_file!s}')
-
-
 def init_ig_client(username: str, password: str) -> Client:
     """
     Initialize the Instagram client.
@@ -110,50 +98,48 @@ def init_ig_client(username: str, password: str) -> Client:
     :param password: password of the account
     :return: Initialized Instagram client
     """
-    settings_file = f'{settings_file_dir}/settings_{username}.json'
-    api = Client(username, password, on_login=lambda x: onlogin_callback(x, settings_file))
+    settings_file = Path(f'{settings_file_dir}/settings_{username}.json')
+    client = Client()
+
     try:
-        if not os.path.isfile(settings_file):
-            # Create a new login if existing settings is not found
-            logger.error(f'Unable to find file: {settings_file!s}')
-            logger.info(f'Attempt new login request for {username}...')
-            api.login()
-        else:
-            # Reuse saved settings
-            with open(settings_file) as file_data:
-                cached_settings = json.load(file_data, object_hook=from_json)
-            logger.info(f'Reusing settings: {settings_file!s}')
-            api = Client(username, password, settings=cached_settings)
-        logger.info('Logged in')
-        return api
-    except ClientTwoFactorRequiredError as e:
-        logger.info('2FA Required')
+        if os.path.isfile(settings_file):
+            # Reuse login settings if existing settings is found
+            logger.info('Existing settings found')
+            client.load_settings(settings_file)
+        logger.info(f'Attempt to login with username {username}...')
+        client.login(username=username, password=password)
+    except TwoFactorRequired:
         # 2FA authentication is required
-        response = json.loads(e.error_response)
-        two_factor_info = response['two_factor_info']
-        two_factor_identifier = two_factor_info['two_factor_identifier']
+        logger.info('Login failed: 2FA required')
         verification_code = input('Enter verification code: ')
         try:
-            api.login2fa(two_factor_identifier, verification_code)
-            logger.info('Logged in')
-            return api
+            # Attempt to login with verification code
+            logger.info(f'Attempt to login with username {username}...')
+            client.login(username=username, password=password, verification_code=verification_code)
         except ClientError as e:
-            logger.error(e.error_response)
+            logger.error(e.response)
     except ClientError as e:
-        logger.error(e.error_response)
+        logger.error(e.response)
+
+    if client:
+        # Save login settings
+        client.dump_settings(settings_file)
+
+    return client
 
 
-def update_ig_profile(api: Client, profile: dict):
+def update_ig_profile(client: Client, profile: dict):
     """
     Update Instagram profile of the current user.
 
-    :param api: Instagram client
+    :param client: Instagram client
     :param profile: Dict of profile to replace
     :return:
     """
     try:
-        res = api.edit_profile(**profile)
-        logger.info(f'status: {res["status"]}')
+        res = client.account_edit(**profile)
+        if res:
+            logger.info('Instagram profile successfully updated')
     except Exception as e:
         logger.error(e)
 
